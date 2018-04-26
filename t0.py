32fd18c953ab8ecb1bbd0381908b7d5a8dbf8a6a
solewyx <38742684+solewyx@users.noreply.github.com>
2018-04-26 10:54:33 +0800

    test the python list 001
    test the python list 001,detail.

---
+++ t0.py
@@ -0,0 +1,16 @@
+import os 
+
+def test():
+	print("hello world")
+	print("nihao wangyouxin")
+
+	l = [1,3,9,8,42]
+	for k in l:
+		print("%d"%(k));
+
+	# f = open(file,"wt")
+	# f.writelines("wangyouxin,nihao")
+	# f.close();
+	
+	
+test()
